bf18e723c1c94a26db674ea161d4885fbc0e6a30
dzhelez2903 <dzhelez@yahoo.com>
2021-02-14 19:32:27 +0200

    add google map

--- js/main.js
+++ js/main.js
@@ -69,7 +69,7 @@ $(".our_team__slider").slick({
 });
 
 /*set mobile menu*/
-
+/*
 function burgerMenu(selector) {
   let menu = $(selector);
   let button = menu.find(
@@ -99,3 +99,53 @@ function burgerMenu(selector) {
 }
 
 burgerMenu(".header__mobile_menu");
+*/
+function initMap() {
+		let element = document.getElementById('map');
+		let options = {
+			zoom: 18, 
+			center: {lat: 49.23034135336109, lng: 28.445238399796878},
+		};
+
+		let myMap = new google.maps.Map(element, options);
+
+    let styles = [
+      
+    ]
+
+		let markers = [
+			{ 
+				coordinates: {lat: 49.23094568221329, lng: 28.44296109898287},
+				info: '<h3>Винница</h3><br><img src="../img/img/origami.png"><br><p>Beetroot Academy</p>'
+			},
+		];
+
+    $.getJSON("../json/style.json", function(data) {
+      map.setOptions({styles: data});
+  });
+
+		for(let i = 0; i < markers.length; i++) {
+			addMarker(markers[i]);
+		}
+
+		function addMarker(properties) {
+			let marker = new google.maps.Marker({
+				position: properties.coordinates,
+				map: myMap
+			});
+
+			if(properties.image) {
+				marker.setIcon(properties.image);
+			}
+
+			if(properties.info) {
+				let InfoWindow = new google.maps.InfoWindow({
+					content: properties.info
+				});
+
+				marker.addListener('click', function(){
+					InfoWindow.open(myMap, marker);
+				});
+			}
+		}
+	}
