1691f7b0f77206a85395d68f9da98d00b40eb1c1
dzhelez2903 <dzhelez@yahoo.com>
2021-02-19 17:41:03 +0200

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -15,20 +15,13 @@ $(".our_team__slider").slick({
   dots: false,
   arrows: true,
   infinite: true,
+  /*centerPadding: '30px',*/
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1300,
-      settings: {
-        slidesToShow: 2,
-        arrows: false,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 1200,
       settings: {
         slidesToShow: 2,
         arrows: false,
